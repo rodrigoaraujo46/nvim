73664211b4ace7d98b0de1761f45e179401a1d70
rodrigoaraujo46 <rodrigo46.dev@gmail.com>
2025-11-16 14:49:52 +0000

    Better harpoon

--- lua/animus/lazy/harpoon.lua
+++ lua/animus/lazy/harpoon.lua
@@ -7,18 +7,13 @@ return {
 
         harpoon:setup()
 
-        vim.keymap.set("n", "<leader>A", function() harpoon:list():prepend() end)
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 
-        vim.keymap.set("n", "<leader>ha", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<leader>hs", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<leader>hd", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<leader>hf", function() harpoon:list():select(4) end)
-        vim.keymap.set("n", "<leader><C-a>", function() harpoon:list():replace_at(1) end)
-        vim.keymap.set("n", "<leader><C-s>", function() harpoon:list():replace_at(2) end)
-        vim.keymap.set("n", "<leader><C-d>", function() harpoon:list():replace_at(3) end)
-        vim.keymap.set("n", "<leader><C-f>", function() harpoon:list():replace_at(4) end)
+        vim.keymap.set("n", "<A-j>", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<A-k>", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<A-l>", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<A-;>", function() harpoon:list():select(4) end)
     end
 }
