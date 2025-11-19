18ecbc57f5b90da7d5467fe266bec0487b38a925
rodrigoaraujo46 <rodrigo46.dev@gmail.com>
2025-11-19 20:28:52 +0000

    User blink.cmp tag version for stability

--- lua/pack/blink.lua
+++ lua/pack/blink.lua
@@ -1,5 +1,5 @@
 vim.pack.add({
-	{ src = 'https://github.com/saghen/blink.cmp' },
+	{ src = 'https://github.com/saghen/blink.cmp',            version = vim.version.range('1.*') },
 	{ src = 'https://github.com/rafamadriz/friendly-snippets' }
 })
 
