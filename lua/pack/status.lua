4da567d162b59ca52ca72ce328a39d7ca18bc4a2
rodrigoaraujo46 <rodrigo46.dev@gmail.com>
2025-11-23 15:34:42 +0000

    Change branch icon

--- lua/pack/status.lua
+++ lua/pack/status.lua
@@ -1,9 +1,9 @@
 vim.pack.add({
-	{ src = 'https://github.com/nvim-tree/nvim-web-devicons' },
-	{ src = 'https://github.com/nvim-lualine/lualine.nvim' }
+	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
+	{ src = "https://github.com/nvim-lualine/lualine.nvim" }
 })
 
-require 'lualine'.setup({
+require "lualine".setup({
 	options = {
 		component_separators = '',
 		section_separators = { left = '', right = '' },
@@ -12,7 +12,7 @@ require 'lualine'.setup({
 		lualine_a = {
 			{ 'mode', separator = { left = '' }, right_padding = 2 }
 		},
-		lualine_b = { 'filename', 'branch' },
+		lualine_b = { 'filename', { 'branch', icon = '' } },
 		lualine_c = {},
 		lualine_x = {},
 		lualine_y = { 'filetype', 'progress' },
