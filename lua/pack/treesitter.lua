91197fca27bec60d6b0a41dc3bd06fbaa66f262f
rodrigoaraujo46 <rodrigo46.dev@gmail.com>
2025-12-21 20:48:58 +0000

    Treesitter main is now the default

--- lua/pack/treesitter.lua
+++ lua/pack/treesitter.lua
@@ -1,5 +1,5 @@
 vim.pack.add({
-	{ src = 'https://github.com/nvim-treesitter/nvim-treesitter',             version = 'main' },
+	{ src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
 	{ src = 'https://github.com/nvim-treesitter/nvim-treesitter-textobjects', version = 'main' }
 })
 
