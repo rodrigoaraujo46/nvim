146f922051a0835bd679459778552094c6ddce0e
rodrigoaraujo46 <rodrigo46.dev@gmail.com>
2026-01-19 02:05:04 +0000

    turn todo-comments signs on

--- lua/pack/todo.lua
+++ lua/pack/todo.lua
@@ -1,7 +1,7 @@
 vim.pack.add({ { src = 'https://github.com/folke/todo-comments.nvim' } })
 
 require('todo-comments').setup({
-	signs = false,
+	signs = true,
 	keywords = {
 		FIX = { icon = ' ' },
 		TODO = { icon = '󰡕 ' },
