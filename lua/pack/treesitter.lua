90da7089a122c27b2f75de6c620bf2e948bd51ac
rodrigoaraujo46 <rodrigo46.dev@gmail.com>
2026-06-06 17:17:13 +0100

    add conditional textobjects and refactor treesitter keymaps

--- lua/pack/treesitter.lua
+++ lua/pack/treesitter.lua
@@ -21,20 +21,29 @@ vim.api.nvim_create_autocmd("FileType", {
 	desc = "install parsers and start treesitter",
 })
 
-local textobjects = require("nvim-treesitter-textobjects.select")
+local sto = require("nvim-treesitter-textobjects.select").select_textobject
+local modes = { "x", "o" }
 
-vim.keymap.set({ "x", "o" }, "af", function()
-	textobjects.select_textobject("@function.outer", "textobjects")
+vim.keymap.set(modes, "af", function()
+	sto("@function.outer", "textobjects")
 end, { desc = "Around function textobject" })
 
-vim.keymap.set({ "x", "o" }, "if", function()
-	textobjects.select_textobject("@function.inner", "textobjects")
+vim.keymap.set(modes, "if", function()
+	sto("@function.inner", "textobjects")
 end, { desc = "Inside function textobject" })
 
-vim.keymap.set({ "x", "o" }, "ac", function()
-	textobjects.select_textobject("@class.outer", "textobjects")
+vim.keymap.set(modes, "ac", function()
+	sto("@class.outer", "textobjects")
 end, { desc = "Around class textobject" })
 
-vim.keymap.set({ "x", "o" }, "ic", function()
-	textobjects.select_textobject("@class.inner", "textobjects")
+vim.keymap.set(modes, "ic", function()
+	sto("@class.inner", "textobjects")
 end, { desc = "Inside class textoject" })
+
+vim.keymap.set(modes, "ii", function()
+	sto("@conditional.inner", "textobjects")
+end, { desc = "Inside conditional textoject" })
+
+vim.keymap.set(modes, "ai", function()
+	sto("@conditional.outer", "textobjects")
+end, { desc = "Around conditional textobject" })
